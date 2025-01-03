328bb17ac45ba1ab0772b3364bc1d6299960ac44
bkodinsky <35776285+bkodinsky@users.noreply.github.com>
2025-01-03 11:17:04 -0500

    Configure AWS Amplify with outputs from amplify_outputs.json in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
 
-// Temporarily comment out aws-exports config until file is created
-// import awsconfig from "./aws-exports";
-// Amplify.configure(awsconfig);
+Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+ 
