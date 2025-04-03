ba4c60776aa8ee51e98680287bfda6354f380db0
bkodinsky <35776285+bkodinsky@users.noreply.github.com>
2025-04-03 11:09:23 -0400

    Remove unnecessary whitespace in resource.ts for cleaner code.

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -48,9 +48,6 @@ export const data = defineData({
   },
 });
 
-
-
-
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
